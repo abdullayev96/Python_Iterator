0d9dd8bc33c86ecc17b00866790941db85a680bb
Burhon Abdullaev <abdullaev_b@turonbank.uz>
2025-07-08 14:50:03 +0500

    iterator and for

---
+++ fisrt_iterator.py
@@ -0,0 +1,79 @@
+#############################   Iterator
+
+# nums = [10, 20, 30]
+# it = iter(nums)      # iterator obyektini yaratamiz
+#
+# print(next(it))      # 10
+# print(next(it))      # 20
+# print(next(it))      # 30
+# print(next(it))      # StopIteration (endi element qolmadi)
+
+
+
+
+
+################  for bilan
+
+
+# for i in nums:
+#     print(i)
+
+
+
+####################################   5 DAN 20 GACHA TOQ SONLARNI OLISH
+
+# class MyRange:
+#     def __init__(self, start, end):
+#         self.current = start
+#         self.end = end
+#
+#     def __iter__(self):
+#         return self
+#
+#     def __next__(self):
+#         if self.current >= self.end:
+#             raise StopIteration
+#         val = self.current
+#         self.current += 1
+#         return val
+#
+#
+# for num in MyRange(5, 10):
+#     print(num)
+
+
+# n = int(input("N son:"))
+#
+# for i in range(n, 20):
+#     if i%2 == 1:
+#         print(i)
+
+
+##################   1 mlrd  sonlarni chiqarmoqchi bolsak
+
+
+# numbers = [i for i in range(1, 1_000_000_000) if i % 2 == 1]
+# print(numbers)
+
+
+########################    Iteratorni foydasi
+
+class OddRange:
+    def __init__(self, start, end):
+        self.current = start if start % 2 == 1 else start + 1
+        self.end = end
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current >= self.end:
+            raise StopIteration
+        val = self.current
+        self.current += 2
+        return val
+
+for num in OddRange(1, 1_000_000_000):
+    if num > 100000:
+        break
+    print(num)
